28245c1316eee3b6d6e753d693b6252a98514838
sahil <sahilpatni95@outlook.com>
2021-02-17 15:36:45 +0530

    contours

--- Basics/contours.py
+++ Basics/contours.py
@@ -1,9 +1,11 @@
 import cv2 as cv
 import numpy as np
 
-git_url = 'https://raw.githubusercontent.com/sahilpatni95/Opencv-projects/main/Data/Photos/cats.jpg'
+#git_url = 'https://raw.githubusercontent.com/sahilpatni95/Opencv-projects/main/Data/Photos/cats.jpg'
+local_url = 'C:/Users/sahil/PycharmProjects/Opencv-projects/Data/Photos/cat.jpg'
 
-img = cv.imread(git_url)
+
+img = cv.imread(local_url)
 cv.imshow('Cats', img)
 
 blank = np.zeros(img.shape, dtype = 'uint8')
@@ -28,5 +30,21 @@ print(f'{len(contours)} contour(s) found!')
 
 cv.drawContours(blank, contours, -1, (0,0,255), 1)
 cv.imshow('Contours Drawn', blank)
+
+# cv.waitKey(0)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Simple Thresholding
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY )
+cv.imshow('Simple Thresholded', thresh)
+
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV )
+cv.imshow('Simple Thresholded Inverse', thresh_inv)
+
+# Adaptive Thresholding
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 9)
+cv.imshow('Adaptive Thresholding', adaptive_thresh)
 
 cv.waitKey(0)
