b93e6b11cddc0b39d2621d05ecb175925b6e61f5
sahil <sahilpatni95@outlook.com>
2021-02-17 13:39:37 +0530

    add video reading code

--- Basics/read.py
+++ Basics/read.py
@@ -1,7 +1,18 @@
 import cv2 as cv
 
-img = cv.imread('Data/Photos/cat.jpg')
+# Reading Image
+# img = cv.imread('https://raw.githubusercontent.com/sahilpatni95/Opencv-projects/main/Data/Photos/cat.jpg')
+# cv.imshow('Cat', img)
+# cv.waitKey(0)
 
-#cv.imshow('Cat', img)
 
-cv.waitKey(20)
+# Reading Videos
+capture = cv.VideoCapture('https://raw.githubusercontent.com/sahilpatni95/Opencv-projects/main/Data/Videos/dog.mp4')
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
+
