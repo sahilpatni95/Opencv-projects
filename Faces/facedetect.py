303c348e9877dc49b95f2003e9729bcb9aeccd75
sahil <sahilpatni95@outlook.com>
2021-02-18 11:40:31 +0530

    All Faces opencv files

---
+++ Faces/facedetect.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+img = cv.imread('Data/group 1.jpg')
+cv.imshow('Group', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+haar_cascade = cv.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 1)
+
+print(f'Number of faces found = {len(faces_rect)}')
+
+for (x, y, w, h) in faces_rect:
+    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness = 2)
+
+cv.imshow('Detected Faces', img)
+
+cv.waitKey(0)
